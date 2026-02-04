d7a011687523b55b24ef1d58724536da59328622
xurxodev <xurxosf@gmail.com>
2026-02-04 09:13:01 +0100

    Fix send sms serialization

--- app/src/spIP/java/org/dhis2/usescases/sms/data/model/D2Constant.kt
+++ app/src/spIP/java/org/dhis2/usescases/sms/data/model/D2Constant.kt
@@ -2,10 +2,6 @@ package org.dhis2.usescases.sms.data.model
 
 import kotlinx.serialization.Serializable
 
-//TODO: Technical debt
-// fix bug to release in urgent mode
-// Create D2ConstantDTO with serializable and
-// map from D2ConstantDTO to D2Constant in repository
 @Serializable
 data class D2Constant(
   val id: String,
